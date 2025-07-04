9a9766f01172ff8b96f6e7d974eca28176e8b068
massodasuki <massodasuki@gmail.com>
2025-07-05 01:33:48 +0800

    add ApiTags searchQuery

--- elesen-api-v1/src/search/search.controller.ts
+++ elesen-api-v1/src/search/search.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get, Query} from '@nestjs/common';
 import { SearchService } from './search.service';
+import { ApiQuery, ApiTags, ApiOkResponse  } from '@nestjs/swagger';
 
+@ApiTags('Search Query')
 @Controller('/api/v1/applications/search')
 export class SearchController {
   constructor(private readonly searchService: SearchService) {}
 
   @Get('ic')
-    searchByIC(
+  searchByIC(
       @Query('q') q: string,
       @Query('page') page = 1,
       @Query('limit') limit = 10,
